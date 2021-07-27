5c0e40401a804bcc17018ad4a8863879c981ec38
日瀬 健人 <kentnewbie@hotmail.com>
2021-07-28 01:50:57 +0900

    T

---
+++ DockBar/DockBar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace DockBar
+{
+    public partial class DockBar : UserControl
+    {
+        public Form[] Forms { get; set; }
+
+        public DockStyle Dock { get; set; }
+
+        public DockBar()
+        {
+            Dock = DockStyle.Left;
+            Width = 100;
+            //Height = Parent.Height;
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+    }
+}
