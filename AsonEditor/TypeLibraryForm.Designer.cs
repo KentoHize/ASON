742b5914e8e64f00e6adbd96150d30a1c966d6b2
日瀬 健人 <kentnewbie@hotmail.com>
2021-08-10 00:30:17 +0900

    T

--- AsonEditor/TypeLibraryForm.Designer.cs
+++ AsonEditor/TypeLibraryForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace AsonEditor
 {
-    partial class TreenodeMapForm
+    partial class TypeLibraryForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,7 +29,6 @@ namespace AsonEditor
         /// </summary>
         private void InitializeComponent()
         {
-            this.treeView1 = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
             // CloseButton
@@ -44,30 +43,17 @@ namespace AsonEditor
             // 
             this.AutoHideButton.FlatAppearance.BorderSize = 0;
             // 
-            // treeView1
-            // 
-            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.treeView1.Location = new System.Drawing.Point(0, 35);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(790, 750);
-            this.treeView1.TabIndex = 0;
-            // 
-            // TreenodeMapForm
+            // TypeLibraryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 780);
-            this.Controls.Add(this.treeView1);
-            this.Name = "TreenodeMapForm";
-            this.Text = "TreenodeForm1";
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "TypeLibraryForm";
+            this.Text = "TypeLibraryForm";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TreeView treeView1;
     }
 }
